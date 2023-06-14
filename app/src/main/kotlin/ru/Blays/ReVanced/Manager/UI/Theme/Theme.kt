e0722c8108117da0bc19561ac7fad2c9fce29797
B1ays <tima.b.planshet@gmail.com>
2023-06-14 16:13:27 +0300

    Add simple Material theme for Crash Handler Activity

--- app/src/main/kotlin/ru/Blays/ReVanced/Manager/UI/Theme/Theme.kt
+++ app/src/main/kotlin/ru/Blays/ReVanced/Manager/UI/Theme/Theme.kt
@@ -5,8 +5,10 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -15,6 +17,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 import ru.blays.revanced.Elements.Util.BuildedTheme
+
 
 @SuppressLint("ObsoleteSdkInt")
 @Composable
@@ -38,6 +41,40 @@ fun ReVancedManagerTheme(
         darkTheme && isAmoledTheme -> buildedTheme.darkColorScheme.copy(background = Color.Black)
         darkTheme -> buildedTheme.darkColorScheme
         else -> buildedTheme.lightColorScheme
+    }
+
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+@SuppressLint("ObsoleteSdkInt")
+@Composable
+fun ReVancedManagerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+
+    val colorScheme = when {
+        dynamicColor && (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> darkColorScheme()
+        else -> lightColorScheme()
     }
 
     val view = LocalView.current
