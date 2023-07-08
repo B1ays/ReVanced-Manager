d9d9aa7b5a4911ca4357e1b3f5787fd1f00f2f0f
B1ays <tima.b.planshet@gmail.com>
2023-07-09 00:59:32 +0300

    Make all theme and colors change animated

--- app/src/main/kotlin/ru/Blays/ReVanced/Manager/UI/Theme/Theme.kt
+++ app/src/main/kotlin/ru/Blays/ReVanced/Manager/UI/Theme/Theme.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -33,7 +34,7 @@ fun ReVancedManagerTheme(
     content: @Composable () -> Unit
 ) {
 
-    val colorScheme = when {
+    /*val colorScheme = when {
         dynamicColor && (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
             val context = LocalContext.current
             val darkColorScheme = dynamicDarkColorScheme(context)
@@ -67,6 +68,98 @@ fun ReVancedManagerTheme(
             )
         }
         else -> buildedTheme.lightColorScheme
+    }*/
+
+    val colorScheme = with(
+        when {
+            dynamicColor && (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) && !isAmoledTheme -> {
+                val context = LocalContext.current
+                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            }
+            dynamicColor && (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) && isAmoledTheme -> {
+                val context = LocalContext.current
+                if (darkTheme) dynamicDarkColorScheme(context).copy(
+                    background = Color.Black,
+                    surface = Color.Black,
+                    surfaceVariant = Color.Black,
+                    surfaceContainer = Color.Black,
+                    surfaceTint = Color.Black
+                ) else dynamicLightColorScheme(context)
+            }
+            darkTheme && isAmoledTheme -> buildedTheme.darkColorScheme.copy(
+                background = Color.Black,
+                surface = Color.Black,
+                surfaceVariant = Color.Black,
+                surfaceContainer = Color.Black,
+                surfaceTint = Color.Black
+            )
+            darkTheme -> buildedTheme.darkColorScheme
+            else -> {
+                buildedTheme.lightColorScheme
+            }
+        }
+    ) {
+        val primary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.primary)
+        val onPrimary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onPrimary)
+        val primaryContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.primaryContainer)
+        val onPrimaryContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onPrimaryContainer)
+        val inversePrimary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.inversePrimary)
+        val secondary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.secondary)
+        val onSecondary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onSecondary)
+        val secondaryContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.secondaryContainer)
+        val onSecondaryContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onSecondaryContainer)
+        val tertiary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.tertiary)
+        val onTertiary by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onTertiary)
+        val tertiaryContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.tertiaryContainer)
+        val onTertiaryContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onTertiaryContainer)
+        val background by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.background)
+        val onBackground by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onBackground)
+        val surface by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.surface)
+        val onSurface by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onSurface)
+        val surfaceVariant by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.surfaceVariant)
+        val onSurfaceVariant by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onSurfaceVariant)
+        val surfaceTint by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.surfaceTint)
+        val inverseSurface by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.inverseSurface)
+        val inverseOnSurface by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.inverseOnSurface)
+        val error by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.error)
+        val onError by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onError)
+        val errorContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.errorContainer)
+        val onErrorContainer by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.onErrorContainer)
+        val outline by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.outline)
+        val outlineVariant by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.outlineVariant)
+        val scrim by animateColorAsState(animationSpec = spring(stiffness = 300F), targetValue = this.scrim)
+
+        ColorScheme(
+            primary,
+            onPrimary,
+            primaryContainer,
+            onPrimaryContainer,
+            inversePrimary,
+            secondary,
+            onSecondary,
+            secondaryContainer,
+            onSecondaryContainer,
+            tertiary,
+            onTertiary,
+            tertiaryContainer,
+            onTertiaryContainer,
+            background,
+            onBackground,
+            surface,
+            onSurface,
+            surfaceVariant,
+            onSurfaceVariant,
+            surfaceTint,
+            inverseSurface,
+            inverseOnSurface,
+            error,
+            onError,
+            errorContainer,
+            onErrorContainer,
+            outline,
+            outlineVariant,
+            scrim
+        )
     }
 
 
