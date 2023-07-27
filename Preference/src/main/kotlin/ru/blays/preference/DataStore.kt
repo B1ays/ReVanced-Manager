c19b8b0d74b3cac30a40ff6436b3f95efee28635
B1ays <tima.b.planshet@gmail.com>
2023-07-27 22:18:13 +0300

    Change Datastore extension visibility

--- Preference/src/main/kotlin/ru/blays/preference/DataStore.kt
+++ Preference/src/main/kotlin/ru/blays/preference/DataStore.kt
@@ -3,6 +3,6 @@ package ru.blays.preference
 import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
 
-val Context.dataStore by preferencesDataStore(
+internal val Context.dataStore by preferencesDataStore(
     name = "Settings"
 )
