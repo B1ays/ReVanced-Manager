6848312337660a561efefb8db513893300a1fb82
B1ays <tima.b.planshet@gmail.com>
2023-08-04 09:37:18 +0300

    Rename package

--- SimpleDocument/src/main/kotlin/ru/blays/simpledocument/SimpleDocumentTree.kt
+++ SimpleDocument/src/main/kotlin/ru/blays/simpledocument/SimpleDocumentTree.kt
@@ -1,12 +1,16 @@
 package ru.blays.simpledocument
 
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.os.ParcelFileDescriptor
 import android.provider.DocumentsContract
+import android.provider.DocumentsContract.Document.FLAG_VIRTUAL_DOCUMENT
 import ru.blays.simpledocument.Utils.canRead
 import ru.blays.simpledocument.Utils.canWrite
+import ru.blays.simpledocument.Utils.closeQuietly
 import ru.blays.simpledocument.Utils.documentType
 import ru.blays.simpledocument.Utils.exists
 import ru.blays.simpledocument.Utils.getFlags
@@ -18,16 +22,16 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.util.Date
 
-class SimpleDocumentSingle internal constructor(
+class SimpleDocumentTree internal constructor(
     override var uri: Uri,
     private val context: Context
 ): SimpleDocument() {
 
+    private val contentResolver = context.contentResolver
+
     internal constructor(parent: SimpleDocument, uri: Uri, context: Context): this(uri, context) {
         this.parent = parent
     }
-
-    private val contentResolver = context.contentResolver
 
     override var parent: SimpleDocument? = null
 
@@ -38,7 +42,6 @@ class SimpleDocumentSingle internal constructor(
             DocumentsContract.Document.COLUMN_DISPLAY_NAME,
             null
         )
-
     override val type: String?
         get() = documentType(context, uri)
     override val path: String?
@@ -57,7 +60,7 @@ class SimpleDocumentSingle internal constructor(
         get() = isDocument && getFlags(
             context,
             uri
-        ) and DocumentsContract.Document.FLAG_VIRTUAL_DOCUMENT != 0
+        ) and FLAG_VIRTUAL_DOCUMENT != 0
     override val lastModifiedLong: Long
         get() = queryForLong(
             context,
@@ -67,12 +70,10 @@ class SimpleDocumentSingle internal constructor(
         )
     override val lastModifiedDate: Date
         get() = TODO("Not yet implemented")
-
     override val canRead: Boolean
         get() = canRead(context, uri)
     override val canWrite: Boolean
         get() = canWrite(context, uri)
-
     override val inputStream: FileInputStream?
         get() = try {
             val fileDescriptor = contentResolver.openFileDescriptor(uri, "rw")
@@ -87,11 +88,47 @@ class SimpleDocumentSingle internal constructor(
         } catch (e: Exception) {
             null
         }
-    override val parcelFileDescriptor: ParcelFileDescriptor?
-        get() = contentResolver.openFileDescriptor(uri, "rw")
+    override val parcelFileDescriptor: ParcelFileDescriptor
+        get() = throw RuntimeException("Can't read parcel file descriptor")
 
     override val documents: Array<SimpleDocument?>
-        get() = emptyArray()
+        get() {
+            val resolver: ContentResolver = context.contentResolver
+            val childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(
+                uri,
+                DocumentsContract.getDocumentId(uri)
+            )
+            val results = ArrayList<Uri>()
+            var cursor: Cursor? = null
+            try {
+                cursor = resolver.query(
+                    childrenUri,
+                    arrayOf(
+                        DocumentsContract.Document.COLUMN_DOCUMENT_ID
+                    ),
+                    null,
+                    null,
+                    null
+                )
+                while (cursor?.moveToNext() == true) {
+                    val documentId = cursor.getString(0)
+                    val documentUri = DocumentsContract.buildDocumentUriUsingTree(
+                        uri,
+                        documentId
+                    )
+                    results.add(documentUri)
+                }
+            } catch (_: Exception) {
+            } finally {
+                closeQuietly(cursor)
+            }
+            val result = results.toTypedArray<Uri>()
+            val resultFiles: Array<SimpleDocument?> = arrayOfNulls(result.size)
+            result.forEachIndexed { index, uri ->
+                resultFiles[index] = SimpleDocumentTree(this, uri, context)
+            }
+            return resultFiles
+        }
 
     override fun delete(): Boolean {
         return try {
@@ -120,14 +157,40 @@ class SimpleDocumentSingle internal constructor(
         }
     }
 
-    override fun createFolder(name: String): SimpleDocument? = null
-    override fun createDocument(name: String, extension: String, mimeType: String): SimpleDocument? = null
-    override fun findFile(fullName: String): SimpleDocument? = null
+    override fun createFolder(name: String): SimpleDocument? {
+        val result = createFile(
+            context,
+            uri,
+            DocumentsContract.Document.MIME_TYPE_DIR,
+            name
+        )
+        return if (result != null) SimpleDocumentTree(this, result, context) else null
+    }
+
+    override fun createDocument(name: String, extension: String, mimeType: String): SimpleDocument? {
+        val result = createFile(
+            context,
+            uri,
+            mimeType,
+            name + extension
+        )
+        return if (result != null) SimpleDocumentSingle(this, result, context) else null
+    }
+
+    override fun findFile(fullName: String): SimpleDocument? {
+        return documents.firstOrNull {
+            it?.name?.contains(fullName) == true
+        }
+    }
+
     override fun getOrCreateDocument(
         name: String,
         extension: String,
         mimeType: String
-    ): SimpleDocument? = null
+    ): SimpleDocument? {
+        return findFile(name)
+            ?: createDocument(name, extension, mimeType)
+    }
 
     override fun copyTo(file: File): Boolean {
         val inputStream = inputStream ?: return false
@@ -149,6 +212,25 @@ class SimpleDocumentSingle internal constructor(
 
     override fun open() {
         val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(intent)
+    }
+
+    private fun createFile(
+        context: Context,
+        uri: Uri,
+        mimeType: String,
+        displayName: String
+    ): Uri? {
+        return try {
+            DocumentsContract.createDocument(
+                context.contentResolver,
+                uri,
+                mimeType,
+                displayName
+            )
+        } catch (e: Exception) {
+            null
+        }
     }
 }
