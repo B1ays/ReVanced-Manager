9349deece8b6fe7f9f3bc843de74ac7f76658511
B1ays <tima.b.planshet@gmail.com>
2023-08-04 11:15:06 +0300

    Bump version to 1.0.6 (7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "ru.Blays.ReVanced.Manager"
         minSdk = 26
         targetSdk = 33
-        versionName = "1.0.5 beta"
-        versionCode = 6
+        versionName = "1.0.6"
+        versionCode = 7
 
         proguardFiles(
             getDefaultProguardFile("proguard-android-optimize.txt"),
