1be9c711408b7eddaa413aea8a681e089b01b993
B1ays <tima.b.planshet@gmail.com>
2023-08-06 14:34:32 +0300

    Update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/versions/9/previous-compilation-data.bin"
         }
     }
     lint {
